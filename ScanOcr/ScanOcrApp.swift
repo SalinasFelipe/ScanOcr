710af567de9466e3facb691611e1f9085e868e1b
David Daniel Rodriguez <david.rodriguez@wiedii.co>
2024-06-26 14:06:34 -0500

    Initial Commit

---
+++ ScanOcr/ScanOcrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanOcrApp.swift
+//  ScanOcr
+//
+//  Created by ddr on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScanOcrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
